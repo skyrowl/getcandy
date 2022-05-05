fceabc5d86d23b11161c7ed568f21ec09e5b7a79
Alec Ritson <hello@itsalec.co.uk>
2022-05-05 10:51:29 +0100

    Add postcode lookup

--- packages/pro/shipping/src/Resolvers/ShippingZoneResolver.php
+++ packages/pro/shipping/src/Resolvers/ShippingZoneResolver.php
@@ -4,6 +4,7 @@ namespace GetCandy\Shipping\Resolvers;
 
 use InvalidArgumentException;
 use GetCandy\Models\Country;
+use GetCandy\Shipping\DataTransferObjects\PostcodeLookup;
 use GetCandy\Shipping\Models\ShippingZone;
 use Illuminate\Support\Collection;
 
@@ -77,6 +78,42 @@ class ShippingZoneResolver
     }
 
     /**
+     * Set the postcode to use when resolving.
+     *
+     * @param string $postcode
+     *
+     * @return self
+     */
+    public function postcode(PostcodeLookup $postcodeLookup): self
+    {
+        $this->postcodes = collect([$postcodeLookup]);
+        $this->types->push('postcodes');
+
+        return $this;
+    }
+
+    /**
+     * Set the postcodes to use for resolving.
+     *
+     * @param Collection $postcodes
+     *
+     * @return self
+     */
+    public function postcodes(Collection $postcodes): self
+    {
+        // Check we haven't got models that aren't countries.
+        throw_if(
+            $postcodes->filter(fn($postcode) => get_class($postcode) != PostcodeLookup::class)->count(),
+            throw new InvalidArgumentException()
+        );
+
+        $this->postcodes = $postcodes;
+        $this->types->push('postcodes');
+
+        return $this;
+    }
+
+    /**
      * Return the shipping zones based on the criteria.
      *
      * @return Collection
@@ -85,24 +122,30 @@ class ShippingZoneResolver
     {
         $query = ShippingZone::query();
 
-        if ($this->countries->count()) {
-            $query->whereHas('countries', function ($query) {
-                $query->whereIn('country_id', $this->countries->pluck('id'));
-            });
+        $query->where(function ($builder) {
+            if ($this->countries->count()) {
+                $builder->whereHas('countries', function ($query) {
+                    $query->whereIn('country_id', $this->countries->pluck('id'));
+                });
+            }
+            if ($this->postcodes->count()) {
+                $builder->orWhereHas('postcodes', function ($query) {
+                    foreach ($this->postcodes as $postcodeLookup) {
+                        $query->orWhere(function ($qb) use ($postcodeLookup) {
+                            $qb->wherePostcode($postcodeLookup->postcode)
+                                ->whereCountryId($postcodeLookup->country->id);
+                        });
+                    }
+                });
+            }
+        });
+
+        if (!$this->countries->count() && !$this->postcodes->count()) {
+            $this->types = collect(
+                ['unrestricted']
+            );
         }
 
-        if ($this->countries->count()) {
-            $this->type = 'countries';
-        }
-
-        if ($this->postcodes->count()) {
-            $this->type = 'postcodes';
-        }
-
-        if ($this->type) {
-            $query->whereType($this->type);
-        }
-
-        return $query->get();
+        return $query->whereIn('type', $this->types)->get();
     }
 }
