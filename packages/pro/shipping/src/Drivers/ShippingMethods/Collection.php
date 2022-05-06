9b2b6802938d6c0d3f59a7502e14f663b8a67749
Alec Ritson <hello@itsalec.co.uk>
2022-05-06 11:23:22 +0100

    Add test for collection

--- packages/pro/shipping/src/Drivers/ShippingMethods/Collection.php
+++ packages/pro/shipping/src/Drivers/ShippingMethods/Collection.php
@@ -2,8 +2,10 @@
 
 namespace GetCandy\Shipping\Drivers\ShippingMethods;
 
+use GetCandy\DataTypes\Price;
 use GetCandy\DataTypes\ShippingOption;
 use GetCandy\Models\Cart;
+use GetCandy\Shipping\DataTransferObjects\ShippingOptionRequest;
 use GetCandy\Shipping\Http\Livewire\Components\ShippingMethods\Collection as ShippingMethodsCollection;
 use GetCandy\Shipping\Interfaces\ShippingMethodInterface;
 use GetCandy\Shipping\Models\ShippingMethod;
@@ -43,9 +45,24 @@ class Collection implements ShippingMethodInterface
         return (new ShippingMethodsCollection())->getName();
     }
 
-    public function getShippingOption(Cart $cart): ShippingOption|null
+    public function resolve(ShippingOptionRequest $shippingOptionRequest): ShippingOption|null
     {
-        return null;
+        $shippingMethod = $shippingOptionRequest->shippingMethod;
+        $cart = $shippingOptionRequest->cart;
+
+        return new ShippingOption(
+            name: $shippingMethod->name,
+            description: $shippingMethod->description,
+            identifier: $shippingMethod->getIdentifier(),
+            price: new Price(
+                value: 0,
+                currency: $cart->currency,
+                unitQty: 1
+            ),
+            taxClass: $shippingMethod->getTaxClass(),
+            taxReference: $shippingMethod->getTaxReference(),
+            option: $shippingMethod->getOption(),
+        );
     }
 
     /**
---
+++ packages/pro/shipping/tests/Unit/Drivers/ShippingMethods/CollectionTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace GetCandy\Shipping\Tests\Unit\Drivers\ShippingMethods;
+
+use GetCandy\DataTypes\ShippingOption;
+use GetCandy\Models\Currency;
+use GetCandy\Models\TaxClass;
+use GetCandy\Shipping\DataTransferObjects\ShippingOptionRequest;
+use GetCandy\Shipping\Drivers\ShippingMethods\Collection;
+use GetCandy\Shipping\Models\ShippingMethod;
+use GetCandy\Shipping\Models\ShippingZone;
+use GetCandy\Shipping\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use GetCandy\Shipping\Tests\TestUtils;
+
+/**
+ * @group getcandy.shipping.drivers
+ */
+class CollectionTest extends TestCase
+{
+    use RefreshDatabase, TestUtils;
+
+    /** @test */
+    public function can_get_free_shipping()
+    {
+        $currency = Currency::factory()->create([
+            'default' => true,
+        ]);
+
+        TaxClass::factory()->create([
+            'default' => true,
+        ]);
+
+        $shippingZone = ShippingZone::factory()->create([
+            'type' => 'countries',
+        ]);
+
+        $shippingMethod = ShippingMethod::factory()->create([
+            'shipping_zone_id' => $shippingZone->id,
+            'driver' => 'free-shipping',
+            'data' => [],
+        ]);
+
+        $cart = $this->createCart($currency, 500);
+
+        $driver = new Collection();
+
+        $request = new ShippingOptionRequest(
+            cart: $cart,
+            shippingMethod: $shippingMethod
+        );
+
+        $shippingOption = $driver->resolve($request);
+
+        $this->assertInstanceOf(ShippingOption::class, $shippingOption);
+
+        $this->assertEquals(0, $shippingOption->price->value);
+    }
+}
