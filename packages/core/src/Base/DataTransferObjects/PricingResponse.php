cf699a4631fdfcba95eedf9b3bcada6df6d5648e
Alec Ritson <hello@itsalec.co.uk>
2022-05-06 10:40:38 +0100

    Support null values

--- packages/core/src/Base/DataTransferObjects/PricingResponse.php
+++ packages/core/src/Base/DataTransferObjects/PricingResponse.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Collection;
 class PricingResponse
 {
     public function __construct(
-        public Price $matched,
-        public Price $base,
+        public ?Price $matched = null,
+        public ?Price $base = null,
         public Collection $tiered,
         public Collection $customerGroupPrices,
     ) {
