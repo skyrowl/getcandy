3ea6fdbebf3642018362cad81aea76a4f4ae673c
Alec Ritson <hello@itsalec.co.uk>
2022-05-09 09:16:18 +0100

    Clean up

--- packages/pro/shipping/src/ShippingServiceProvider.php
+++ packages/pro/shipping/src/ShippingServiceProvider.php
@@ -21,11 +21,6 @@ use Livewire\Livewire;
 
 class ShippingServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        //
-    }
-
     public function boot(ShippingModifiers $shippingModifiers)
     {
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'shipping');
@@ -35,7 +30,7 @@ class ShippingServiceProvider extends ServiceProvider
         $slot->addItem(function ($item) {
             $item->name(
                 __('shipping::index.menu_item')
-            )->handle('hub.tickets')
+            )->handle('hub.shipping')
             ->route('hub.shipping.index')
             ->icon('truck');
         });
@@ -44,57 +39,12 @@ class ShippingServiceProvider extends ServiceProvider
             ShippingModifier::class
         );
 
-        // // Register our payment type.
-        // Payments::extend('opayo', function ($app) {
-        //     return $app->make(OpayoPaymentType::class);
-        // });
-
-        // $this->app->singleton(OpayoInterface::class, function ($app) {
-        //     return $app->make(Opayo::class);
-        // });
-
-        // $this->mergeConfigFrom(__DIR__."/../config/opayo.php", "getcandy.opayo");
 
         $this->loadRoutesFrom(__DIR__.'/../routes/hub.php');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-        // Blade::directive('opayoScripts', function () {
-        //     $url = 'https://pi-test.sagepay.com/api/v1/js/sagepay.js';
-
-        //     $manifest = json_decode(file_get_contents(__DIR__.'/../dist/mix-manifest.json'), true);
-
-        //     $jsUrl = asset('/vendor/opayo'.$manifest['/opayo.js']);
-
-        //     if (config('services.opayo.env', 'test') == 'live') {
-        //         $url = 'https://pi-live.sagepay.com/api/v1/js/sagepay.js';
-        //     }
-
-        //     $manifest = json_decode(file_get_contents(__DIR__.'/../dist/mix-manifest.json'), true);
-
-        //     $jsUrl = asset('/vendor/getcandy'.$manifest['/opayo.js']);
-
-        //     return  <<<EOT
-        //         <script src="{$jsUrl}"></script>
-        //         <script src="{$url}"></script>
-        //     EOT;
-        // });
-
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'shipping');
-
-        // dd(realpath(__DIR__.'/../lang'));
-
-        // $this->publishes([
-        //     __DIR__."/../config/opayo.php" => config_path("getcandy/opayo.php"),
-        // ], 'getcandy.opayo.config');
-
-        // $this->publishes([
-        //     __DIR__.'/../resources/views' => resource_path('views/vendor/getcandy'),
-        // ], 'getcandy.opayo.components');
-
-        // $this->publishes([
-        //     __DIR__.'/../dist' => public_path('vendor/getcandy'),
-        // ], 'getcandy.opayo.public');
 
         // // Register the stripe payment component.
 
