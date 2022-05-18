e0b6831f0b5702491ffc91d224a005da07433872
Alec Ritson <hello@itsalec.co.uk>
2022-05-18 13:22:54 +0100

    Tweak actions

--- packages/admin/resources/views/partials/orders/actions.blade.php
+++ packages/admin/resources/views/partials/orders/actions.blade.php
@@ -1,6 +1,6 @@
 @if($this->transactions->count())
-<button
-  class="inline-flex items-center px-4 py-2 font-bold transition border border-transparent border-gray-200 rounded hover:bg-white bg-gray-50 hover:border-gray-200"
+<x-hub::button theme="gray" size="sm"
+
   type="button"
   wire:click.prevent="$set('showRefund', true)"
 >
@@ -16,7 +16,7 @@
     {{ __('adminhub::components.orders.show.refund_btn') }}
   @endif
 
-</button>
+</x-hub::button>
 @endif
 
 @if($this->requiresCapture)
