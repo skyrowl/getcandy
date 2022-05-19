350b374a4dca54b9c977695cb936be8165d478d5
Alec Ritson <hello@itsalec.co.uk>
2022-05-19 15:01:07 +0100

    Translations

--- packages/pro/shipping/lang/en/index.php
+++ packages/pro/shipping/lang/en/index.php
@@ -2,5 +2,7 @@
 
 return [
     'menu_item' => 'Shipping',
+    'title' => 'Shipping Zones',
     'no_results' => 'There are no Shipping Zones in the system',
+    'add_zone_btn' => 'Add shipping zone',
 ];
--- packages/pro/shipping/resources/views/index.blade.php
+++ packages/pro/shipping/resources/views/index.blade.php
@@ -1,17 +1,17 @@
 <div class="flex-col px-8 space-y-4 md:px-12">
   <div class="items-center justify-between md:flex">
     <strong class="block text-lg font-bold md:text-2xl">
-      Shipping Zones
+      {{ __('shipping::index.title') }}
     </strong>
     <x-hub::button tag="a" href="{{ route('hub.shipping-zone.create') }}">
-      Add shipping zone
+      {{ __('shipping::index.add_zone_btn') }}
     </x-hub::button>
   </div>
    <div class="flex-col space-y-4">
       <x-hub::table>
         <x-slot name="head">
           <x-hub::table.heading>
-            Name
+            {{ __('adminhub::global.name') }}
           </x-hub::table.heading>
           <x-hub::table.heading></x-hub::table.heading>
         </x-slot>
@@ -24,7 +24,7 @@
 
               <x-hub::table.cell>
                 <a href="{{ route('hub.shipping.shipping-zone.show', $zone->id) }}" class="text-indigo-500 hover:underline">
-                    Edit
+                  {{ __('adminhub::global.edit') }}
                 </a>
               </x-hub::table.cell>
           </x-hub::table.row>
@@ -33,38 +33,10 @@
               {{ __('shipping::index.no_results') }}
             </x-hub::table.no-results>
           @endforelse
-          {{-- @forelse($currencies as $currency)
-            <x-hub::table.row>
-              <x-hub::table.cell>
-                <span class="block w-2 h-2 border rounded-full @if($currency->default) bg-green-400 border-green-600 @endif"></span>
-              </x-hub::table.cell>
-              <x-hub::table.cell>
-                {{ $currency->name }}
-              </x-hub::table.cell>
-              <x-hub::table.cell>
-                {{ $currency->code }}
-              </x-hub::table.cell>
-              <x-hub::table.cell>
-                {{ $currency->exchange_rate }}
-              </x-hub::table.cell>
-              <x-hub::table.cell>
-                <x-hub::icon :ref="$currency->enabled ? 'check' : 'x'" :class="$currency->enabled ? 'text-green-500' : 'text-red-500'" style="solid" />
-              </x-hub::table.cell>
-              <x-hub::table.cell class="text-right">
-                <a href="{{ route('hub.currencies.show', $currency->id) }}" class="text-indigo-500 hover:underline">
-                    {{ __('adminhub::settings.currencies.index.table_row_action_text') }}
-                  </a>
-              </x-hub::table.cell>
-            </x-hub::table.row>
-          @empty
-            <x-hub::table.no-results>
-              {{ __('adminhub::settings.currencies.index.no_results') }}
-            </x-hub::table.no-results>
-          @endforelse --}}
         </x-slot>
       </x-hub::table>
       <div>
-        {{-- {{ $currencies->links() }} --}}
+        {{ $this->shippingZones->links() }}
       </div>
     </div>
 </div>
